75818a1fc898243f578c5d0c87b392197ea16646
Clebson Moura <clebson.sh@gmail.com>
2024-09-22 16:22:59 -0300

    INNER JOIN

---
+++ intermediate.sql
@@ -0,0 +1,13 @@
+-- INNER JOIN
+SELECT ed.employee_id,
+       ed.first_name,
+       ed.last_name,
+       ed.age,
+       ed.gender,
+       ed.birth_date,
+       es.occupation,
+       es.salary
+FROM employee_demographics AS ed
+    INNER JOIN employee_salary AS es
+        ON ed.employee_id = es.employee_id;
+
