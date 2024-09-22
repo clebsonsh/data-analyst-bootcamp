e381bad078ba8724643b8728a43a28d62312d30b
Clebson Moura <clebson.sh@gmail.com>
2024-09-22 16:23:25 -0300

    RIGHT JOIN

--- intermediate.sql
+++ intermediate.sql
@@ -24,3 +24,16 @@ FROM employee_demographics AS ed
     LEFT OUTER JOIN employee_salary AS es
         ON ed.employee_id = es.employee_id;
 
+-- RIGHT OUTER JOIN
+SELECT es.employee_id,
+       es.first_name,
+       es.last_name,
+       ed.age,
+       ed.gender,
+       ed.birth_date,
+       es.occupation,
+       es.salary
+FROM employee_demographics AS ed
+    RIGHT OUTER JOIN employee_salary AS es
+        ON ed.employee_id = es.employee_id;
+
