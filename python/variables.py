e3f244994895fb7eeff5296da5d610d2607ed467
Clebson Moura <clebson.sh@gmail.com>
2024-10-06 20:14:15 -0300

    python basic variables

---
+++ python/variables.py
@@ -0,0 +1,95 @@
+# variable declaration
+# python infer variable type
+# in this case we have a int
+x = 22
+
+# print x in the terminal
+print(x)
+
+# print x type in the terminal
+print(type(x))
+
+# assign a string to y
+y = "Mint Chocolate Chip"
+
+# print y in the terminal
+print(y)
+
+# print y type in the terminal
+print(type(y))
+
+# assign another string to y
+y = "Chocolate"
+
+# print y new value in the terminal
+print(y)
+
+# we can also assign multiple values to multiple variables
+x, y, z = "Chocolate", "Vanilla", "Rocky Road"
+
+# print x, y, and z values in the terminal
+print(x)
+print(y)
+print(z)
+
+# we can also assign one value to multiple variables
+x = y = z = "Root Beer Float"
+
+# print x, y, and z values in the terminal
+print(x)
+print(y)
+print(z)
+
+# assign a list to ice_creams
+ice_creams = ["Chocolate", "Vanilla", "Rocky Road"]
+
+# we can take the values from a list and assign to multiple variables
+x, y, z = ice_creams
+
+# print x, y, and z values in the terminal
+print(x)
+print(y)
+print(z)
+
+# There are a few away to name variables
+# Camel Case
+testVaribleCase = "Vanilla Swirl"
+
+# Pascal Case
+TestVaribleCase = "Vanilla Swirl"
+
+# Snake Case - mostly used in python world
+test_varible_case = "Vanilla Swirl"
+
+# Use + to concatenate strings, we can only concatenate strings
+x = "Ice Cream is my favorite" + "."
+
+# print x in the terminal
+print(x)
+
+# We also can concatenate in the print statement
+x = "Ice Cream"
+y = " is "
+z = "my favorite."
+
+# concatenate x, y, z and print in the terminal
+print(x + y + z)
+
+# Use + to add to numbers
+x = 5 + 2
+
+# print x in the terminal
+print(x)
+
+# We alse can add in the print statement
+x, y, z = 1, 2, 3
+
+# add x, y, z and print in the terminal
+print(x + y + z)
+
+# We can print multiple types of variables in one print statement
+x = "Ice Cream"
+y = 2
+
+# print x and y in the terminal
+print(x, y)
