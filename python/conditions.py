0820eab33aedf6dd256948ad441813aeda400439
Clebson Moura <clebson.sh@gmail.com>
2024-10-07 21:30:41 -0300

    python conditions

---
+++ python/conditions.py
@@ -0,0 +1,15 @@
+# If - Elif - Else Statements
+if 25 > 10:
+    print("It worked!")
+
+if 25 < 10:
+    print("It worked!")
+else:
+    print("It did not worked...")
+
+if 25 < 10:
+    print("It worked!")
+elif 25 < 30:
+    print("Elif worked!")
+else:
+    print("It did not worked...")
